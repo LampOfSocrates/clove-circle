f39771b65ef782bd3022b55a58ee702bc62f38eb
LampOfSocrates <LampOfSocrates@gmail.com>
2026-03-26 21:37:21 +0000

    Link resources infographics tab to gallery page

--- tests/resources_case_study_test.py
+++ tests/resources_case_study_test.py
@@ -7,7 +7,9 @@ assert "Biorefinery: PHA Biocomposite Production" in resources_html
 assert 'href="case_studies/case-study-pha-biocomposite.html"' in resources_html
 assert "Laterite NHM Processing" in resources_html
 assert 'href="case_studies/case-study-laterite-lca-tea.html"' in resources_html
+assert 'href="infographics/infographics.html"' in resources_html
 assert 'href="references.html"' in resources_html
+assert 'id="infographics-link"' in resources_html
 assert "Launch Case Study" in resources_html
 assert "Open Case Study" in resources_html
 assert 'id="refList"' not in resources_html
